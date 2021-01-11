de33d8f9e189dfd7a26835731deb01c954faa5d0
Zawar Haider <zawarhaider783@gmail.com>
2021-01-11 16:00:30 +0500

    Agent Reg form fields added.

--- src/components/Agents/Registration/Registration.js
+++ src/components/Agents/Registration/Registration.js
@@ -36,17 +36,57 @@ const Registration = (props) => {
 
     return (
             <Grid container>
-                <Grid item xs={6}>
+                <Grid item xs={6}>g
                     <Card variant="outlined">
                         <CardContent>
                             <form className={classes.form} noValidate autoComplete="off">
-                                <TextField id="standard-basic" label="Standard" />
-                                <TextField id="standard-basic" label="Standard" />
-                                <TextField id="standard-basic" label="Standard" />
-                                <TextField id="standard-basic" label="Standard" />
-                                <TextField id="standard-basic" label="Standard" />
-                                <TextField id="standard-basic" label="Standard" />
-                                <TextField id="standard-basic" label="Standard" />
+                                <TextField required id="filled-required" label="Name:" variant="outlined" />
+                                <TextField required id="standard-basic" label="CNIC #" variant="filled"/>
+                                <TextField required id="standard-basic" label="Mobile #" variant="filled"/>
+                                <TextField required id="standard-basic" label="Telephone #" variant="filled"/>
+                                <TextField required id="standard-basic" label="Date of Birth:" variant="filled"/>
+                                <TextField required id="standard-basic" label="CNIC Expiry Date:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Gender:" variant="filled"/>
+                                <TextField required id="filled-textarea"label="Current Address"
+                                  placeholder="Placeholder"
+                                  multiline variant="filled" />
+                                <TextField required id="filled-textarea"label="Permanent Address"
+                                  placeholder="Placeholder"
+                                  multiline variant="filled" />
+                                <TextField required id="standard-basic" label="Nationality" variant="filled"/>
+                                <TextField required id="standard-basic" label="City:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Tehsil:" variant="filled"/>
+                                <TextField required id="standard-basic" label="District:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Province:" variant="filled"/>
+                                <TextField id="standard-basic" label="NTN # (if any)" variant="filled"/>
+                                <TextField id="standard-basic" label="E-mail: (if any)" variant="filled"/>
+                                <TextField id="standard-basic" label="Qualification:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Exclusivity:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Father's Name:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Mother's Name:" variant="filled"/>
+                                <TextField required id="standard-basic" label="NOK Name:" variant="filled"/>
+                                <TextField required id="standard-basic" label="NOK CNIC #" variant="filled"/>
+                                <TextField required id="standard-basic" label="NOK Relationship:" variant="filled"/>
+                                <TextField required id="standard-basic" label="NOK Contact #" variant="filled"/>
+                                <TextField required id="standard-basic" label="NOK Address:" variant="filled"/>
+                                <TextField id="standard-basic" label="Bank Name:" variant="filled"/>
+                                <TextField id="standard-basic" label="Branch:" variant="filled"/>
+                                <TextField id="standard-basic" label="Account Title:" variant="filled"/>
+                                <TextField id="standard-basic" label="Account #" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Type:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Name:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Owner Name:" variant="filled"/>
+                                <TextField required id="filled-textarea"label="Business Address"
+                                  placeholder="Placeholder"
+                                  multiline variant="filled" />
+                                <TextField required id="standard-basic" label="Business City:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Tehsil:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business District:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Province:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Contact #" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Latitude:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Business Longitude:" variant="filled"/>
+                                <TextField required id="standard-basic" label="Avg Monthly Income:" variant="filled"/>
                             </form>
                         </CardContent>
                         <CardActions>
